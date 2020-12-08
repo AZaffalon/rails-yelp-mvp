ed64af64fec6b64f7f40e76acc140147aca4208e
alexis zaffalon <alexiszaffalon@live.fr>
2020-12-08 21:23:14 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating Restaurants..."
+leon = { name: "léon de bruxelle", address: "Plaisir", phone_number: "666", category: "fruit de mer" }
+mcdo = { name: 'mcdo', address: 'Saint-brice', phone_number: '777', category: 'fast-food' }
+buffalo = { name: 'buffalo grill', address: 'Moisselles', phone_number: '888', category: 'steakhouse' }
+iresto = { name: 'iresto', address: 'Ecouen', phone_number: '999', category: 'fast-food' }
+ritz = { name: 'ritz', address: 'Paris', phone_number: '111', category: 'gastronomie' }
+puts "Finished!"
